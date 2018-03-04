a51380b3a88189c17eeb368433a8b4dc0f3a0fd8
Liu Xinchen <liuxinchen1997@163.com>
2018-03-04 19:03:17 +0800

    modify files

--- app/src/main/java/com/example/kemingxiu/test/BatteryActivity.java
+++ app/src/main/java/com/example/kemingxiu/test/BatteryActivity.java
@@ -6,13 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.nev.utils.ListViewForScrollView;
 
 public class BatteryActivity extends AppCompatActivity {
+
+    private String[] data = {
+            "aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,5 +33,11 @@ public class BatteryActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                BatteryActivity.this, android.R.layout.simple_list_item_1, data
+        );
+        ListView listView = findViewById(R.id.battery_list_view);
+        listView.setAdapter(adapter);
     }
 }
