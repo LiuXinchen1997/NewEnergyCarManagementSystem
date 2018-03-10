0408a94791450ff3d4782b7952a12fa3cfd83947
Liu Xinchen <liuxinchen1997@163.com>
2018-03-10 22:33:56 +0800

    modify files

--- app/src/main/java/com/nev/domain/Battery.java
+++ app/src/main/java/com/nev/domain/Battery.java
@@ -5,5 +5,93 @@ package com.nev.domain;
  */
 
 public class Battery {
+    private String batteryNum;
+    private String batteryBrand;
+    private String batteryVersion;
+    private String carNum;
+    private boolean isWarning;
+    private boolean isUse;
+    private boolean isFailure;
 
+    public String getBatteryNum() {
+        return batteryNum;
+    }
+
+    public void setBatteryNum(String batteryNum) {
+        this.batteryNum = batteryNum;
+    }
+
+    public String getBatteryBrand() {
+        return batteryBrand;
+    }
+
+    public void setBatteryBrand(String batteryBrand) {
+        this.batteryBrand = batteryBrand;
+    }
+
+    public String getBatteryVersion() {
+        return batteryVersion;
+    }
+
+    public void setBatteryVersion(String batteryVersion) {
+        this.batteryVersion = batteryVersion;
+    }
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+    public boolean isWarning() {
+        return isWarning;
+    }
+
+    public void setWarning(boolean warning) {
+        isWarning = warning;
+    }
+
+    public boolean isUse() {
+        return isUse;
+    }
+
+    public void setUse(boolean use) {
+        isUse = use;
+    }
+
+    public boolean isFailure() {
+        return isFailure;
+    }
+
+    public void setFailure(boolean failure) {
+        isFailure = failure;
+    }
+
+    public Battery(String batteryNum, String batteryBrand, String batteryVersion, String carNum, boolean isWarning, boolean isUse, boolean isFailure) {
+        this.batteryNum = batteryNum;
+        this.batteryBrand = batteryBrand;
+        this.batteryVersion = batteryVersion;
+        this.carNum = carNum;
+        this.isWarning = isWarning;
+        this.isUse = isUse;
+        this.isFailure = isFailure;
+    }
+
+    public Battery() {
+    }
+
+    @Override
+    public String toString() {
+        return "Battery{" +
+                "batteryNum='" + batteryNum + '\'' +
+                ", batteryBrand='" + batteryBrand + '\'' +
+                ", batteryVersion='" + batteryVersion + '\'' +
+                ", carNum='" + carNum + '\'' +
+                ", isWarning=" + isWarning +
+                ", isUse=" + isUse +
+                ", isFailure=" + isFailure +
+                '}';
+    }
 }
--- app/src/main/java/com/nev/domain/Car.java
+++ app/src/main/java/com/nev/domain/Car.java
@@ -5,4 +5,225 @@ package com.nev.domain;
  */
 
 public class Car {
+    private String carNum;
+    private String carBrand;
+    private double carryingCapacity;
+    private String dateManu;
+    private int driverNum;
+    private boolean isBreakdown;
+    private double carLng;
+    private double carLat;
+    private boolean isCarLock;
+    private boolean isSeatLock;
+    private boolean isSafetyBelt;
+    private boolean isHandBraking;
+    private double revSpeed;
+    private double driSpeed;
+    private boolean isAlarm;
+    private boolean isPeccancy;
+    private boolean isCollision;
+    private boolean isOverSpeed;
+    private boolean isOverload;
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public double getCarryingCapacity() {
+        return carryingCapacity;
+    }
+
+    public void setCarryingCapacity(double carryingCapacity) {
+        this.carryingCapacity = carryingCapacity;
+    }
+
+    public String getDateManu() {
+        return dateManu;
+    }
+
+    public void setDateManu(String dateManu) {
+        this.dateManu = dateManu;
+    }
+
+    public int getDriverNum() {
+        return driverNum;
+    }
+
+    public void setDriverNum(int driverNum) {
+        this.driverNum = driverNum;
+    }
+
+    public boolean isBreakdown() {
+        return isBreakdown;
+    }
+
+    public void setBreakdown(boolean breakdown) {
+        isBreakdown = breakdown;
+    }
+
+    public double getCarLng() {
+        return carLng;
+    }
+
+    public void setCarLng(double carLng) {
+        this.carLng = carLng;
+    }
+
+    public double getCarLat() {
+        return carLat;
+    }
+
+    public void setCarLat(double carLat) {
+        this.carLat = carLat;
+    }
+
+    public boolean isCarLock() {
+        return isCarLock;
+    }
+
+    public void setCarLock(boolean carLock) {
+        isCarLock = carLock;
+    }
+
+    public boolean isSeatLock() {
+        return isSeatLock;
+    }
+
+    public void setSeatLock(boolean seatLock) {
+        isSeatLock = seatLock;
+    }
+
+    public boolean isSafetyBelt() {
+        return isSafetyBelt;
+    }
+
+    public void setSafetyBelt(boolean safetyBelt) {
+        isSafetyBelt = safetyBelt;
+    }
+
+    public boolean isHandBraking() {
+        return isHandBraking;
+    }
+
+    public void setHandBraking(boolean handBraking) {
+        isHandBraking = handBraking;
+    }
+
+    public double getRevSpeed() {
+        return revSpeed;
+    }
+
+    public void setRevSpeed(double revSpeed) {
+        this.revSpeed = revSpeed;
+    }
+
+    public double getDriSpeed() {
+        return driSpeed;
+    }
+
+    public void setDriSpeed(double driSpeed) {
+        this.driSpeed = driSpeed;
+    }
+
+    public boolean isAlarm() {
+        return isAlarm;
+    }
+
+    public void setAlarm(boolean alarm) {
+        isAlarm = alarm;
+    }
+
+    public boolean isPeccancy() {
+        return isPeccancy;
+    }
+
+    public void setPeccancy(boolean peccancy) {
+        isPeccancy = peccancy;
+    }
+
+    public boolean isCollision() {
+        return isCollision;
+    }
+
+    public void setCollision(boolean collision) {
+        isCollision = collision;
+    }
+
+    public boolean isOverSpeed() {
+        return isOverSpeed;
+    }
+
+    public void setOverSpeed(boolean overSpeed) {
+        isOverSpeed = overSpeed;
+    }
+
+    public boolean isOverload() {
+        return isOverload;
+    }
+
+    public void setOverload(boolean overload) {
+        isOverload = overload;
+    }
+
+    public Car(String carNum, String carBrand, double carryingCapacity, String dateManu, int driverNum, boolean isBreakdown, double carLng, double carLat, boolean isCarLock, boolean isSeatLock, boolean isSafetyBelt, boolean isHandBraking, double revSpeed, double driSpeed, boolean isAlarm, boolean isPeccancy, boolean isCollision, boolean isOverSpeed, boolean isOverload) {
+        this.carNum = carNum;
+        this.carBrand = carBrand;
+        this.carryingCapacity = carryingCapacity;
+        this.dateManu = dateManu;
+        this.driverNum = driverNum;
+        this.isBreakdown = isBreakdown;
+        this.carLng = carLng;
+        this.carLat = carLat;
+        this.isCarLock = isCarLock;
+        this.isSeatLock = isSeatLock;
+        this.isSafetyBelt = isSafetyBelt;
+        this.isHandBraking = isHandBraking;
+        this.revSpeed = revSpeed;
+        this.driSpeed = driSpeed;
+        this.isAlarm = isAlarm;
+        this.isPeccancy = isPeccancy;
+        this.isCollision = isCollision;
+        this.isOverSpeed = isOverSpeed;
+        this.isOverload = isOverload;
+    }
+
+    public Car() {
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carNum='" + carNum + '\'' +
+                ", carBrand='" + carBrand + '\'' +
+                ", carryingCapacity=" + carryingCapacity +
+                ", dateManu='" + dateManu + '\'' +
+                ", driverNum=" + driverNum +
+                ", isBreakdown=" + isBreakdown +
+                ", carLng=" + carLng +
+                ", carLat=" + carLat +
+                ", isCarLock=" + isCarLock +
+                ", isSeatLock=" + isSeatLock +
+                ", isSafetyBelt=" + isSafetyBelt +
+                ", isHandBraking=" + isHandBraking +
+                ", revSpeed=" + revSpeed +
+                ", driSpeed=" + driSpeed +
+                ", isAlarm=" + isAlarm +
+                ", isPeccancy=" + isPeccancy +
+                ", isCollision=" + isCollision +
+                ", isOverSpeed=" + isOverSpeed +
+                ", isOverload=" + isOverload +
+                '}';
+    }
 }
